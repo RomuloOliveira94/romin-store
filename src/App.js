3d0dd90ab8d37e5e2a1a5c735810d930d3af2d9b
Rômulo Oliveira <romuloffall@gmail.com>
2022-05-18 22:24:49 -0300

    initial commit, testing the logics of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState();
+
+  useEffect(() => {
+    const getContent = async () => {
+      const req = await fetch("http://localhost:3000/products");
+      const res = await req.json();
+      setData(res);
+    };
+    getContent();
+  }, []);
+  
+  useEffect(() => {
+    const filters = data && data.anime.filter((val) => {
+      return val.name.toLowerCase().includes("naruto");
+    });
+    setFilteredData(filters);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         APRENDA AFOFAS
-        </a>
-      </header>
+      {data &&
+        data.anime.map((items, index) => (
+          <div key={index}>
+            <img src={items.img} alt="" />
+            <li>{items.name}</li>
+            <li>{items.description}</li>
+            <li>{items.price}</li>
+          </div>
+        ))}
+      {data &&
+        data.games.map((items, index) => (
+          <div key={index}>
+            <img src={items.img} alt="" />
+            <li>{items.name === "naruto"}</li>
+            <li>{items.description}</li>
+            <li>{items.price}</li>
+          </div>
+        ))}
+      {data &&
+        data.comics.map((items, index) => (
+          <div key={index}>
+            <img src={items.img} alt="" />
+            <li>{items.name}</li>
+            <li>{items.description}</li>
+            <li>{items.price}</li>
+          </div>
+        ))}
+        {filteredData &&
+        filteredData.map((val, index) => (
+          <div key={index}>
+            <img src={val.img} alt="" />
+            <li>{val.name}</li>
+            <li>{val.description}</li>
+          </div>
+        ))}
+      {data &&
+        data.anime
+          .filter((val) => {
+            return val.name.toLowerCase().includes("naruto") 
+          })
+          .map((val, key) => {
+            return (
+              <div key={key}>
+                <img src={val.img} alt="" />
+                <li>{val.name}</li>
+                <li>{val.description}</li>
+              </div>
+            );
+          })}
     </div>
   );
 }
