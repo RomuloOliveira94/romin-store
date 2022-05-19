55c828accac88e332acb38d298d0a5badeb2ca83
Rômulo Oliveira <romuloffall@gmail.com>
2022-05-19 17:42:47 -0300

    Added routes for the home page and caregory pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Anime from "./pages/categorys/Anime";
+import Comics from "./pages/categorys/Comics";
+import Games from "./pages/categorys/Games";
 import Home from "./pages/home/Home";
 
 function App() {
-
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/anime" element={<Anime />} />
+          <Route path="/games" element={<Comics />} />
+          <Route path="/comics" element={<Games />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
