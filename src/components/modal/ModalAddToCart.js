4a22661a8b78d7760f741fdb65316ba18a95affe
Rômulo Oliveira <romuloffall@gmail.com>
2022-06-15 12:01:03 -0300

    Implemented a modal to inform cart adding

---
+++ src/components/modal/ModalAddToCart.js
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { Box, Modal, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { style } from "./ModalAddToCart.styled";
+
+const ModalAddToCart = ({ title, description, close, cart }) => {
+  const { open, handleClose } = useContext(CartContext);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box sx={{ textAlign: "center" }}>
+            <Typography id="modal-modal-title" variant="h5" component="h2">
+              Product added to the cart!
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 1, mb: 1 }}>
+              What do you want to do now?
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Link to="/cart">
+              <Button onClick={handleClose}>Cart</Button>
+            </Link>
+            <Button onClick={handleClose}>Keep Buying</Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalAddToCart;
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,22 +3,43 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export function CartContextProvider({ children }) {
+  //modal context
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
   const [cart, setCart] = useState([]);
+
   const handleAddToCart = (product) => {
     setCart([...cart, product]);
+    setOpen(true);
   };
+
   const total = cart.reduce((acc, value) => {
     acc += value.price;
     return acc;
   }, 0);
+
   const removeFromCart = (id) => {
     const remainItens = cart.filter((item) => item.id !== id);
     setCart(remainItens);
   };
 
+  const createOrder = () => {
+    setCart([]);
+  };
+
   return (
     <CartContext.Provider
-      value={{ cart, setCart, total, handleAddToCart, removeFromCart }}
+      value={{
+        cart,
+        setCart,
+        open,
+        total,
+        handleClose,
+        handleAddToCart,
+        removeFromCart,
+        createOrder,
+      }}
     >
       {children}
     </CartContext.Provider>
